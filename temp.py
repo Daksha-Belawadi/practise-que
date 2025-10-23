29b15a1a939d5b652c383af9f043aeb31e831227
Daksha-Belawadi <belawadidaksha@gmail.com>
2025-10-23 14:55:03 +0530

    save to branch1

--- temp.py
+++ temp.py
@@ -3,3 +3,9 @@ if t<20:s="Cold"
 elif t<=30:s="Normal"
 else:s="Hot"
 print(f"Temp:{t}°C Status:{s}")
+t=float(input("Enter °C: "))
+if t<20:s="Cold"
+elif t<=30:s="Normal"
+else:s="Hot"
+f=(t*9/5)+32
+print(f"Temp:{t}°C Status:{s} Fahrenheit:{f}°F")
