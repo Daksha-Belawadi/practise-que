3220e6694951971c759891c01f1ecb60f69c9df9
Daksha-Belawadi <belawadidaksha@gmail.com>
2025-10-23 14:02:20 +0530

    save to branch1

--- Q-1.py
+++ Q-1.py
@@ -4,3 +4,9 @@ elif marks>=75: g="B"
 elif marks>=50: g="C"
 else: g="Fail"
 print("Grade:",g)
+m=[90,80,85,70,85];a=sum(m)/5
+if a>=90:g="A"
+elif a>=75:g="B"
+elif a>=50:g="C"
+else:g="Fail"
+print("Average:",a,"Grade:",g)
